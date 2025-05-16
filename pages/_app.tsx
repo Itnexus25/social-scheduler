1b49cacc46e62c1f8e1f17dc12ef40f7b495048d
Itnexus25 <yahwehsm25@gmail.com>
2025-05-16 13:50:29 +0000

    Update _app.tsx: Use implicit return arrow function with typed pageProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 // pages/_app.tsx
+import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import Head from "next/head";
 import "@/styles/globals.css";
 
-const MyApp = ({ Component, pageProps }) => (
+const MyApp = ({ Component, pageProps }: AppProps) => (
   <ClerkProvider {...pageProps}>
     <Head>
       <title>Social Scheduler</title>
