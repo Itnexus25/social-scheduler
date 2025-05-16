b144061167b653eff26b4718912f9a90e5d14b05
Itnexus25 <yahwehsm25@gmail.com>
2025-05-16 13:48:08 +0000

    Update _app.tsx to use an implicit return arrow function (no explicit return)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,16 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app'
-import { ClerkProvider } from '@clerk/nextjs'
-import Head from 'next/head'
-import '@styles/globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
+import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Social Scheduler</title>
-        <meta name="description" content="Manage your social posts easily" />
-      </Head>
-      <ClerkProvider {...pageProps}>
-        <Component {...pageProps} />
-      </ClerkProvider>
-    </>
-  )
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ClerkProvider {...pageProps}>
+    <Head>
+      <title>Social Scheduler</title>
+      <meta name="description" content="Manage your social posts easily" />
+    </Head>
+    <Component {...pageProps} />
+  </ClerkProvider>
+);
 
-export default MyApp
+export default MyApp;
