fb48eb193049ea5b580948c260323ef79f6f94b5
Itnexus25 <yahwehsm25@gmail.com>
2025-05-16 14:07:36 +0000

    Fix _app.tsx: Provide default fallbacks for Component and pageProps to resolve errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,21 @@
-// pages/_app.tsx
+// src/pages/_app.tsx
+
 import { ClerkProvider, SignedOut, SignInButton } from "@clerk/nextjs";
 import Head from "next/head";
 import "@/styles/globals.css";
 
+// Use a normal function to wrap all code inside a proper function body.
 function MyApp(props) {
-  // Destructure with default fallbacks in case props are missing.
+  // Destructure props (defaults ensure that if these were somehow missing, code won’t error out)
   const { Component = () => null, pageProps = {} } = props;
-
+  
   return (
     <ClerkProvider {...pageProps}>
       <Head>
         <title>Social Scheduler</title>
         <meta name="description" content="Manage your social posts easily" />
       </Head>
-      {/* Optionally render sign-in UI when signed out */}
+      {/* Optionally, render a sign-in UI when the user is signed out */}
       <SignedOut>
         <SignInButton />
       </SignedOut>
